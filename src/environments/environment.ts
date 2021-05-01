2bcaa906d28f235f191e53e7fe4c842e591341d4
Germano Loos <germanoloos@hotmail.com>
2021-05-01 10:27:53 -0300

    Variáveis de produção

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3001',
+  apiUrl: process.env.API_URL || 'http://localhost:3001',
   localStoragePersistence: true
 };
 
